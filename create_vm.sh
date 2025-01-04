fe2013fdeb1697829ef478da000047c2b95e6142
JarodBruce <jarodbruce414@gmail.com>
2025-01-04 22:52:52 +0900

    cahnge:hostnameを変更することができるようにした

--- create_vm.sh
+++ create_vm.sh
@@ -3,12 +3,16 @@ cloudinit_iso_name=`date +"%Y%m%d%I%M%S"`
 
 # nameをユーザーに入力させる
 read -p "Enter the name for the vm: " name
+read -p "Enter the name for the hostname: " hostname
 
 # ディスクのコピーとサイズ変更
 cp --sparse=always /var/lib/libvirt/iso/ubuntu-20.04-server-cloudimg-amd64.img /var/lib/libvirt/images/${name}.img
 qemu-img resize /var/lib/libvirt/images/${name}.img 10G
-#cloudinitファイルを作成
+#cloudinitファイルを作成hostname=AAAA
+echo "$(cat ./meta-data | sed "s/{hostname}/${hostname}/g")" > ./meta-data
+cat meta-data
 genisoimage -output ${cloudinit_iso_name}.iso -volid cidata -joliet -rock ./user-data ./meta-data
+echo "$(cat ./meta-data | sed "s/${hostname}/{hostname}/g")" > ./meta-data
 
 # virt-install コマンドの実行
 sudo virt-install \
